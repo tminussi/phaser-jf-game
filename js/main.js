810c734d98254b92e33745a7b4aed6f90fb2be32
Thales Minussi de Sousa <tminussi@gmail.com>
2018-02-11 16:01:58 -0200

    moving animals with arrows

--- js/main.js
+++ js/main.js
@@ -3,22 +3,18 @@ const game = new Phaser.Game(1280, 780, Phaser.AUTO);
 const GameState = {
     preload: function () {
         this.load.image('background', 'assets/images/background.png');
-        this.load.image('horse', 'assets/images/horse.png');
-        this.load.image('sheep', 'assets/images/sheep.png');
+        this.load.image('horse', 'assets/images/horse1.png');
+        this.load.image('sheep', 'assets/images/sheep1.png');
+        this.load.image('arrow', 'assets/images/arrow.png');
     },
     create: function () {
         this.setScales();
         this.addBackground();
+        this.addLeftArrow();
+        this.addRightArrow();
         this.addAnimals();
     },
     update: function () {
-        // this.animals.forEach((animal) => {
-        //     if (animal.key === 'horse') {
-        //         animal.angle += 1;
-        //     } else {
-        //         animal.angle -= 1;
-        //     }
-        // })
     },
 
     setScales: function () {
@@ -30,6 +26,35 @@ const GameState = {
     addBackground: function () {
         this.background = this.game.add.sprite(0, 0, 'background');
         this.background.scale.setTo(0.5);
+    },
+
+    addLeftArrow: function () {
+        this.leftArrow = this.game.add.sprite(200, this.game.world.centerY, 'arrow');
+        this.leftArrow.anchor.setTo(0.5);
+        this.leftArrow.scale.x = -1;
+        this.leftArrow  .customParams = {
+            direction: {
+                backwards: true
+            }
+        };
+
+        this.leftArrow.inputEnabled = true;
+        this.leftArrow.input.pixelPerfectClick = true;
+        this.leftArrow.events.onInputDown.add(this.switchAnimal, this);
+    },
+
+    addRightArrow: function () {
+        this.rightArrow = this.game.add.sprite(900  , this.game.world.centerY, 'arrow');
+        this.rightArrow.anchor.setTo(0.5);
+        this.rightArrow.customParams = {
+            direction: {
+                forward: true
+            }
+        };
+
+        this.rightArrow.inputEnabled = true;
+        this.rightArrow.input.pixelPerfectClick = true;
+        this.rightArrow.events.onInputDown.add(this.switchAnimal, this);
     },
 
     addAnimals: function() {
@@ -45,16 +70,49 @@ const GameState = {
         ];
         this.animals = this.game.add.group();
         animalImages.forEach((image) => {
-            let animal = this.animals.create(this.game.world.centerX, this.game.world.centerY, image.key);
+            let animal = this.animals.create(-1000, this.game.world.centerY, image.key, 0);
+            animal.customParams = {
+                text: image.key
+            };
             animal.anchor.setTo(0.5);
             animal.inputEnabled = true;
             animal.input.pixelPerfect = true;
             animal.events.onInputDown.add(this.animateAnimal, this);
         });
+        this.currentAnimal = this.animals.next();
+        this.currentAnimal.position.set(this.game.world.centerX-100, this.game.world.centerY);
     },
 
     animateAnimal: function (sprite, event) {
-        console.log('animating ' + sprite.key);
+        console.log(sprite);
+    },
+
+    switchAnimal: function (sprite, event) {
+        let newAnimal;
+        let endX;
+
+        if (sprite.customParams.direction.forward) {
+            newAnimal = this.animals.next();
+            newAnimal.x = (-newAnimal.width / 2);
+            endX = 1280 + this.currentAnimal.width / 2;
+        } else {
+            newAnimal = this.animals.previous();
+            newAnimal.x = 1280 + newAnimal.width / 2;
+            endX = (1280 + this.currentAnimal.width / 2) * -1;
+        }
+        const animalMovement = this.game.add.tween(newAnimal);
+        animalMovement.to({
+            x: this.game.world.centerX - 100
+        }, 1000);
+        animalMovement.start();
+
+        const currentAnimalMovement = this.game.add.tween(this.currentAnimal);
+        currentAnimalMovement.to({
+            x: endX
+        }, 1000);
+        currentAnimalMovement.start();
+
+        this.currentAnimal = newAnimal;
     }
 };
 
